6a4493ab6aa4e74dd1937dea1e5d091d5ec972a1
krussll <raw987@hotmail.com>
2015-08-27 19:48:41 +0100

    Update app.js

--- app.js
+++ app.js
@@ -36,7 +36,9 @@ var server = app.listen(3000, function () {
             {
               data.entities.hashtags.forEach(function(hashtag) {
                 if(hashtag.text.indexOf('?') < 0) {
-                  var strQuery = "INSERT INTO `tagQueue`.`tagQueue` (`id`, `hashtag`, `is_processed`) VALUES (NULL, '" +hashtag.text + "', b'0');";
+                  
+                  
+                  var strQuery = "INSERT INTO \"tagQueue\".\"tagQueue\" (id, hashtag, is_processed) VALUES (NULL, '" + hashtag.text + "', b'0');";
                   client.query(strQuery, function(err, rows){
                     
                   });
